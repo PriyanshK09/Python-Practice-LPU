aa704cb17d7ae7d5dfae55db779c0f7ac2a3c44f
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-01 16:08:46 +0530

    New File Created - Practice 4

---
+++ Practice 4.py
@@ -0,0 +1,11 @@
+#Program to subtract two given Numbers by the User Input
+
+#Taking input the two variables (x and y) and assgnining them a Value
+x = input("Enter 1st Number : ")
+y = input("Enter 2nd Number : ")
+
+#Creating a third variable and subtracting x and y
+z = int(x) - int(y)
+
+#Printing the Sum/Output
+print("The sum of the given numbers is : ", z)
