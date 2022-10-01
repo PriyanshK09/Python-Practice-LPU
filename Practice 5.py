8d926c934b703d982bb86367b333ba2c0aa0a604
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-01 16:16:55 +0530

    modified repo

---
+++ Practice 5.py
@@ -0,0 +1,7 @@
+#Take input from user for Two Numbers (x and y)
+firstVariable = input("Enter 1st Number : ")
+secondVariable = input("Enter 2nd Number : ")
+
+thirdVariable 
+
+print("Swapped Variables are : ", thirdVariable)
