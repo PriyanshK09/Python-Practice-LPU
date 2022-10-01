bf48b7b8890f10dd85e82f8a5419277df912774e
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-01 15:30:33 +0530

    Done with Practice 2 (in class question)

---
+++ Practice 2.py
@@ -0,0 +1,11 @@
+#Program to add two given Numbers
+
+#Creating two variables (x and y) and assgnining them a Value
+x = 5
+y = 8
+
+#Creating a third variable and adding x and y
+z = x + y
+
+#Printing the Sum/Output
+print(z)
