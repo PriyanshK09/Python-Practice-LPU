bc93b165a83d22c0f8ef3cefe7df03c9f28c23ef
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-01 16:38:07 +0530

    Fixed the code of Swapping 2 Numbers

--- Practice 5.py
+++ Practice 5.py
@@ -2,6 +2,13 @@
 number1 = input("Enter 1st Number : ")
 number2 = input("Enter 2nd Number : ")
 
-number1, number2 = number2, number1
-print("number1 =", number1)
-print("number2 =", number2)
+print("Before SWAP, Value of 1st Number is ", number1)
+print("Before SWAP, Value of 2nd Number is ", number2)
+
+#swapping numbers
+number3 = number1
+number1 = number2
+number2 = number3
+
+print("After SWAP, Value of 1st Number is ", number1)
+print("After SWAP, Value of 2nd Number is ", number2)
