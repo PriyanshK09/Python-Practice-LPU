020c0ba70b2db2f600ae7eec0e66c1f14a333fcd
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-15 22:54:01 +0530

    New File Added

---
+++ Practice 20.py
@@ -0,0 +1,21 @@
+#sorting the list means arrainging the number in ascending orders
+
+list = [8, 5, 13, 34, 55, 12, 9, 0, 55, 42, 99, 21, 38]
+# list.sort() : this sort list in ascending order
+list.sort()
+print(list)
+# list.reverse() : this reverse series in descending order
+list.reverse()
+print(list)
+# list.append() : insters a number at the end of the series.
+list.append(27)
+print(list)
+# list.insert() : this insert the number in between
+list.insert(8,30)
+print(list)
+# list.pop() : this removes number which is at index/position no.2
+list.pop(2)
+print(list)
+# list.remove() : this removes the number from the list
+list.remove(98)
+print(list) 
