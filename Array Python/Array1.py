2d435ee35eccec209cb298ad2641f734b524b427
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-16 13:48:25 +0530

    modified Array

--- Array Python/Array1.py
+++ Array Python/Array1.py
@@ -3,3 +3,18 @@
 a = "Hello World"
 print(a[6])
 print(a[9])
+
+#Know the length of a String
+print(len(a))
+
+#IN keyword tells if the given word is in your String/Sentence or not
+if "or" in a :
+    print("Yes! It's there.")
+else :
+    print("No")
+
+#NOT IN keyword tells if given word is not in your String/Sentence
+if "le" not in a :
+    print("No! It's not in Sentence.")
+else :
+    print("It's there.")
