a436bcf19df0a62b07f7d551626b9b17ff564f23
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-10-16 15:28:38 +0530

    Learned about Slicing of Strings

---
+++ Array Python/Slicing 2.py
@@ -0,0 +1,3 @@
+#Negatice Indexing
+sentence2 = "Hello World"
+print(sentence2[-5:-2])
---
+++ Array Python/Slicing1.py
@@ -0,0 +1,17 @@
+sentence = "Hey there! How are you?"
+#print(variable[start, stop])
+#stop is not included
+print(sentence[2:5])
+
+#print(variable[:stop])
+#start from beginning of sentence automatically 
+print(sentence[:5])
+
+#print(variable[start:])
+#start defined and ending set to last letter automatically 
+print(sentence[2:])
+
+#print(variable[:])
+#start and stop at beginning and end of string respectively automatically
+print(sentence[:])
+
