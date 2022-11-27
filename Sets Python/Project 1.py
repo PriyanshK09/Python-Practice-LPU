1402b5775d80d23cad690777e6c5a3735c6b9683
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 16:41:15 +0530

    New File added to the repository

---
+++ Sets Python/Project 1.py
@@ -0,0 +1,6 @@
+# A SET in Python is a collection which is unordered and unindexed. No duplicate members.
+# Denoted by curly brackets "{}"
+
+mySet = {"apple", "banana", "cherry"}
+print(mySet) #Output willnot be in the same order as entered because sets are unordered and unindexed.
+
---
+++ Tuples Python/Project 9.py
@@ -0,0 +1,6 @@
+tuple1 = ("H","E","L","L","O")
+# Output: HELLO
+
+(a,b,c,d,e)=tuple1
+result = a+b+c+d+e
+print(result)
