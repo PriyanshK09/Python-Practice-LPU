e5a1e398b670695ee24d3662e372fd431e33fbfa
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 13:38:27 +0530

    new file:   Tuples Python/Practice 6.py

---
+++ Tuples Python/Practice 6.py
@@ -0,0 +1,12 @@
+tuple1 = (10,20)
+tuple2 = (30,40)
+
+
+# Swap the Tuple Values 
+# Output :
+# tuple1 = (30, 40)
+# tuple2 = (10, 20)
+
+tuple1, tuple2 = tuple2, tuple1
+print("tuple1 =", tuple1)
+print("tuple2 =", tuple2)
