1d736afcc2d3584e2a933a424f2fe0059c38f143
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 13:30:56 +0530

    List and Tuples questions added

---
+++ Tuples Python/Practice 3.py
@@ -0,0 +1,3 @@
+tuples1 = (10,20,30,40,50)
+#Reversing the Tuple
+print("Reverse of the given tuple is :",tuples1[::-1])
---
+++ Tuples Python/Practice 4.py
@@ -0,0 +1,5 @@
+#Make out a Tuple of your Name and Print out the Tuple
+tuple = ("P", "r", "i", "y", "a", "n", "s", "h")
+tuple1 = ("Priyansh",)
+print(tuple)
+print(tuple1)
