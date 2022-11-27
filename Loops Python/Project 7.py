852c71a0508bc4a0cc8b7a0acdf100e168a6c354
Mr. Priyansh <priyanshkhare4905@gmail.com>
2022-11-27 16:18:33 +0530

    New file added to the repository

---
+++ Loops Python/Project 7.py
@@ -0,0 +1,10 @@
+# Take multiple input from the user and store that in a tuple and print the tuple  
+
+a = input("Enter the first name : ")
+b = input("Enter the second name : ")
+c = input("Enter the third name : ")
+d = input("Enter the fourth name : ")
+e = input("Enter the fifth name : ")
+
+tuple1=((a, b, c, d, e))
+print(tuple1)
---
+++ Tuples Python/Readme.py
@@ -0,0 +1,76 @@
+
+
+#tuples - store multiple items in a single variable
+# non homogeneous
+# ordered
+#unchangable / immutable
+# allows duplicate value
+
+
+# myTuple = (1,2,3,4,4)
+# print(len(myTuple))
+# print(myTuple)
+
+
+# tuple1 = ("apple",1, 1.1)
+# print(tuple1)
+
+
+# tuple1 = ("car", "bike", "boat", "jet")
+# print(tuple1[:3])
+
+# tuple1 = ("car", "bike", "boat", "jet")
+# list1 =  list(tuple1)
+# list1.append("cycle")
+# tuple2 = tuple(list1)
+# print(tuple2)
+
+# tuple1 = (10,20,30,40,50)
+# #reverse it
+
+# tuple1 = ("apple", "orange", "pineapple")
+# del tuple1
+# print(tuple1)
+
+
+# tuple1 = (1,2,3,[6,7],4,5)
+# print(tuple1[3][0], tuple1[4])
+
+# tuple1 = (10,20)
+# tuple2 = (30, 40)
+
+#swap the tuples
+#output - 
+# tuple1 : (30,40)
+# tuple2 : (10, 20)
+
+
+#packing and unpacking
+# tuple1 = (1,2,3)
+# (one, two, three) = tuple1
+
+# print(one)
+# print(two)
+# print(three)
+
+
+# tuple1 = ("car", "bike", "boat", "cycle")
+# (item1, *item2) = tuple1
+
+# print(item1)
+# print(item2)
+
+
+#write a program to unpack folowwing tuple into 4 variables
+
+tuple1 = (10,20,30,40,20)
+#count and print occurences of 20
+
+
+# i = 0
+# while i < len(tuple1):
+#     print(tuple1[i])
+#     i += 1
+
+
+
