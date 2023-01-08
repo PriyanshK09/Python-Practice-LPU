06514820835bb0f885e994b1fca9abfd74d5529f
Mr. Priyansh <priyanshkhare4905@gmail.com>
2023-01-08 19:29:13 +0530

    modified:   Project 01.py

--- Project 01.py
+++ Project 01.py
@@ -90,7 +90,7 @@ class Game():
             print("showing cards")
             
     def get_trump(self):
-        self.trump = self.deck.cards[39].suit
+        self.trump = self.deck.cards[0].suit
         print(f"The trump is {self.trump}")
         
     def play_round(self):
@@ -124,4 +124,92 @@ class Game():
         self.play_round()
         
 player1 = Player("Player 1")
-            
+player2 = Player("Player 2")
+player3 = Player("Player 3")
+player4 = Player("Player 4")
+game = Game(player1, player2, player3, player4)
+game.play_game()
+
+# Output
+# Player 1
+# 1 of Spades
+# 2 of Spades
+# 3 of Spades
+# 4 of Spades
+# 5 of Spades
+# 6 of Spades
+# 7 of Spades
+# 8 of Spades
+# 9 of Spades
+# 10 of Spades
+# showing cards
+# Player 2
+# 1 of Clubs
+# 2 of Clubs
+# 3 of Clubs
+# 4 of Clubs
+# 5 of Clubs
+# 6 of Clubs
+# 7 of Clubs
+# 8 of Clubs
+# 9 of Clubs
+# 10 of Clubs
+# showing cards
+# Player 3
+# 1 of Diamonds
+# 2 of Diamonds
+# 3 of Diamonds
+# 4 of Diamonds
+# 5 of Diamonds
+# 6 of Diamonds
+# 7 of Diamonds
+# 8 of Diamonds
+# 9 of Diamonds
+# 10 of Diamonds
+# showing cards
+# Player 4
+# 1 of Hearts
+# 2 of Hearts
+# 3 of Hearts
+# 4 of Hearts
+# 5 of Hearts
+# 6 of Hearts
+# 7 of Hearts
+# 8 of Hearts
+# 9 of Hearts
+# 10 of Hearts
+# showing cards
+# The trump is Spades
+# Round 1
+# Player 1
+# 1 of Spades
+# 2 of Spades
+# 3 of Spades
+# 4 of Spades
+# 5 of Spades
+# 6 of Spades
+# 7 of Spades
+# 8 of Spades
+# 9 of Spades
+# 10 of Spades
+# Played 1 of Spades
+# Player 2
+# 1 of Clubs
+# 2 of Clubs
+# 3 of Clubs
+# 4 of Clubs
+# 5 of Clubs
+# 6 of Clubs
+# 7 of Clubs
+# 8 of Clubs
+# 9 of Clubs
+# 10 of Clubs
+# Played 1 of Clubs
+# Player 3
+# 1 of Diamonds
+# 2 of Diamonds
+# 3 of Diamonds
+# 4 of Diamonds
+# 5 of Diamonds
+# 6 of Diamonds
+#
